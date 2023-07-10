05c325bc0d1aa6906fe42e6d014aa9b469c51c56
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-07-10 14:37:03 -0400

    lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+    if length > 0:
+        fibonacci_list.append(0)
+    if length > 1:
+        fibonacci_list.append(1)
+    for n in range(2, length):
+        acc = (fibonacci_list[-1] + fibonacci_list[-2])
+        fibonacci_list.append(acc)
+    print(fibonacci_list)
